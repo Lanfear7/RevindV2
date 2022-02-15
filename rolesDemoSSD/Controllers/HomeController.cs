cfb4446eda91f20d193fe0f3de248258840b8581
Royce Wee <weeroyce@gmail.com>
2022-02-15 12:04:42 -0800

    api test royce

--- rolesDemoSSD/Controllers/HomeController.cs
+++ rolesDemoSSD/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
 using rolesDemoSSD.Data;
 using rolesDemoSSD.Models;
 using System;
@@ -56,5 +57,42 @@ namespace rolesDemoSSD.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        // api test
+        // Install-Package RestSharp
+        [HttpGet]
+        [Route("Home/getContent")]
+        public async Task<ActionResult> getContent()
+        {
+            string url = "https://api.themoviedb.org/3/movie/76324?api_key=1fa26c8425e1e11c9424d0273f1e98f8";
+
+
+            var client = new RestClient(url);
+            var request = new RestRequest(url, Method.Get);
+            var response = await client.ExecuteAsync(request);
+            string content = response.Content;
+
+            string[] unwantedCharacters = { "\\", "\"", "{", "[", "}", "}" };
+
+            // remove unwanted characters
+            foreach (var unwantedCharacter in unwantedCharacters) {
+                content = content.Replace(unwantedCharacter, "");
+            }
+
+
+            var contentArray = content.Split(",");
+
+            List<string> names = new List<string>();
+            List<string> values = new List<string>();
+            foreach (var contentItem in contentArray) {
+                string[] nameValuePair = contentItem.Split(":");
+         
+                names.Add(nameValuePair[0]);
+                values.Add(nameValuePair[1]);
+            }
+
+            return View();
+        }
+
     }
 }
