b4e8daf42801dd1dcdf3753c29e224af896c9f7d
Lanfear7 <68174240+Lanfear7@users.noreply.github.com>
2022-03-02 16:03:27 -0800

    Login Page/Register is all done

--- rolesDemoSSD/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ rolesDemoSSD/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -59,6 +59,10 @@ namespace rolesDemoSSD.Areas.Identity.Pages.Account
             [Display(Name = "First Name")]
             public string FirstName { get; set; }
 
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
