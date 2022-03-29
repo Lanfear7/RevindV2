be46f4911abad5b36db177f5815c8bd04fbdc247
Lanfear7 <68174240+Lanfear7@users.noreply.github.com>
2022-03-29 14:01:23 -0700

    Added some JS functionality

--- rolesDemoSSD/wwwroot/js/site.js
+++ rolesDemoSSD/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let posterCardArr = document.querySelectorAll(".MovieCard")
+let hiddenPosterCardArr = document.querySelectorAll(".MovieCardHidden")
+let colseBtnArr = document.querySelectorAll('.CloseBtn')
+posterCardArr.forEach((card, i) => {
+    card.addEventListener('click', () => {
+        console.log('clicked', hiddenPosterCardArr[i])
+        if (hiddenPosterCardArr[i].style.display != "block") {
+            console.log('in', hiddenPosterCardArr[i])
+            hiddenPosterCardArr[i].style.display = "block"
+        }
+
+
+    })
+})
+
+colseBtnArr.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        console.log('clicked', hiddenPosterCardArr[i])
+        if (hiddenPosterCardArr[i].style.display == "block") {
+            console.log('in', hiddenPosterCardArr[i])
+            hiddenPosterCardArr[i].style.display = "none"
+        } else {
+            hiddenPosterCardArr[i].style.display = "block"
+        }
+    })
+})
+
